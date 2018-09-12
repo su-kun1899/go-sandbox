fbc3fc3d3e8cb8117ae30d3f3613fbd2486f1415
su-kun1899 <higedrum.coz@gmail.com>
2018-09-12 21:18:45 +0900

    テストケースをテーブルで書く

--- greeting/greeting_test.go
+++ greeting/greeting_test.go
@@ -5,20 +5,72 @@ import (
 	"github.com/su-kun1899/go-sandbox/greeting"
 	"time"
 	"bytes"
+	"io"
 )
 
 func TestGreeting_Do(t *testing.T) {
-	g := greeting.Greeting{
-		Clock: mockClock(t, "2018/08/31 06:00:00"),
+	cases := map[string]struct {
+		writer io.Writer
+		clock  greeting.Clock
+
+		msg       string
+		expectErr bool
+	}{
+		"04時": {
+			writer: new(bytes.Buffer),
+			clock:  mockClock(t, "2018/08/31 04:00:00"),
+			msg:    "おはよう",
+		},
+		"09時": {
+			writer: new(bytes.Buffer),
+			clock:  mockClock(t, "2018/08/31 09:00:00"),
+			msg:    "おはよう",
+		},
+		"10時": {
+			writer: new(bytes.Buffer),
+			clock:  mockClock(t, "2018/08/31 10:00:00"),
+			msg:    "こんにちは",
+		},
+		"16時": {
+			writer: new(bytes.Buffer),
+			clock:  mockClock(t, "2018/08/31 16:00:00"),
+			msg:    "こんにちは",
+		},
+		"17時": {
+			writer: new(bytes.Buffer),
+			clock:  mockClock(t, "2018/08/31 17:00:00"),
+			msg:    "こんばんは",
+		},
+		"03時": {
+			writer: new(bytes.Buffer),
+			clock:  mockClock(t, "2018/08/31 03:00:00"),
+			msg:    "こんばんは",
+		},
 	}
 
-	var buf bytes.Buffer
-	if err := g.Do(&buf); err != nil {
-		t.Error("unexpected error:", err)
-	}
+	for n, tc := range cases {
+		t.Run(n, func(t *testing.T) {
+			g := greeting.Greeting{
+				Clock: tc.clock,
+			}
 
-	if expected, actual := "おはよう", buf.String(); expected != actual {
-		t.Errorf("greeting message want %s but got %s", expected, actual)
+			switch err := g.Do(tc.writer); true {
+			// エラーを期待しているのにエラーが発生していない
+			case err == nil && tc.expectErr:
+				t.Error("expected error did not occur")
+				// エラーは期待していないのにエラーが発生した
+			case err != nil && !tc.expectErr:
+				t.Error("unexpected error:", err)
+			}
+
+			// tc.writerが*byte.Bufferだったら値もチェック
+			if buff, ok := tc.writer.(*bytes.Buffer); ok {
+				msg := buff.String()
+				if msg != tc.msg {
+					t.Errorf("greeting message want %s but got %s", tc.msg, msg)
+				}
+			}
+		})
 	}
 }
 
