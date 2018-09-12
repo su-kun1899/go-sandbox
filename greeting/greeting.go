45983959375f6e5df1d440d2ab80eaaf79bf7a01
su-kun1899 <higedrum.coz@gmail.com>
2018-09-12 20:35:16 +0900

    テストしやすいコードにする
    現在時刻を取得する部分を差し替え可能にする
    インタフェースの実装を簡単にするためのfunction typeも作成
    インタフェースをフィールドとして持つ構造体を作成
    書き込み先を自由に変えられるようにする

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -2,17 +2,45 @@ package greeting
 
 import (
 	"time"
+	"io"
 	"fmt"
 )
 
-func Do() {
-	h := time.Now().Hour()
+type Clock interface {
+	Now() time.Time
+}
+
+type ClockFunc func() time.Time
+
+func (f ClockFunc) Now() time.Time {
+	return f()
+}
+
+type Greeting struct {
+	Clock Clock
+}
+
+func (g *Greeting) now() time.Time {
+	if g.Clock == nil {
+		return time.Now()
+	}
+	return g.Clock.Now()
+}
+
+func (g *Greeting) Do(w io.Writer) error {
+	h := g.now().Hour()
+	var msg string
 	switch {
 	case h >= 4 && h <= 9:
-		fmt.Println("おはよう")
+		msg = "おはよう"
 	case h >= 10 && h <= 16:
-		fmt.Println("こんにちは")
+		msg = "こんにちは"
 	default:
-		fmt.Println("こんばんは")
+		msg = "こんばんは"
 	}
+	_, err := fmt.Fprint(w, msg)
+	if err != nil {
+		return err
+	}
+	return nil
 }
