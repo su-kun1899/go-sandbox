544ca7476346cdb2da65d28d1b75f8dabba10dbb
su-kun1899 <higedrum.coz@gmail.com>
2018-10-16 01:02:33 +0900

    Refactor

--- file/file.go
+++ file/file.go
@@ -25,7 +25,7 @@ func ReadFromLast(fileName string) ([]string, error) {
 	fmt.Printf("fileName: %v, fileSize: %v\n", fileInfo.Name(), fileInfo.Size())
 
 	cursor := int64(0)
-	line := make([]byte, 0)
+	line := ""
 	for i := int64(0); i < fileInfo.Size(); i++ {
 		cursor--
 
@@ -34,18 +34,13 @@ func ReadFromLast(fileName string) ([]string, error) {
 		char := make([]byte, 1)
 		file.Read(char)
 
-		fmt.Printf("char[0]: %v\n", char[0])
 		if i != 0 && char[0] == 10 {
 			fmt.Println("改行だ")
 			break
 		}
-		line = append(line, char[0])
+		line = fmt.Sprintf("%s%s", string(char), line)
+		fmt.Printf("line: %v", line)
 	}
 
-	// 逆向きソートする
-	for left, right := 0, len(line)-1; left < right; left, right = left+1, right-1 {
-		line[left], line[right] = line[right], line[left]
-	}
-
-	return []string{string(line)}, nil
+	return []string{line}, nil
 }
