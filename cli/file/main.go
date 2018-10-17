db56ee2ae4af2898bade2adeecc5a53d220f70c8
su-kun1899 <higedrum.coz@gmail.com>
2018-10-17 20:53:49 +0900

    とりあえずファイルのパスを解決

--- cli/file/main.go
+++ cli/file/main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/su-kun1899/go-sandbox/file"
 	"os"
+	"path/filepath"
 )
 
 func main()  {
 	writer := os.Stdout
+	errWriter := os.Stderr
 
-	fmt.Fprint(writer,"hello\n")
+	// TODO パスはどうにかしないと
+	fileName, _ := filepath.Abs("file/testdata/append.txt")
+
+	strings, err := file.ReadFromLast(fileName)
+	if err != nil {
+		fmt.Fprintf(errWriter,"%v\n", err)
+		return
+	}
+
+	fmt.Fprintf(writer,"%v\n", strings[0])
 }
