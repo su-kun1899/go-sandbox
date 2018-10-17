2ebe6533df2663aa40ebcb2fd1637b0431ffb0d8
su-kun1899 <higedrum.coz@gmail.com>
2018-10-17 21:35:16 +0900

    file closeして読み直せば最新のサイズが取れる？

--- cli/file/main.go
+++ cli/file/main.go
@@ -3,22 +3,68 @@ package main
 import (
 	"fmt"
 	"github.com/su-kun1899/go-sandbox/file"
+	"io"
 	"os"
 	"path/filepath"
+	"time"
 )
 
-func main()  {
+func main() {
 	writer := os.Stdout
 	errWriter := os.Stderr
 
 	// TODO パスはどうにかしないと
-	fileName, _ := filepath.Abs("file/testdata/append.txt")
-
-	strings, err := file.ReadFromLast(fileName)
+	fileName, err := filepath.Abs("file/testdata/append.txt")
 	if err != nil {
-		fmt.Fprintf(errWriter,"%v\n", err)
+		fmt.Fprintf(errWriter, "%v\n", err)
 		return
 	}
 
-	fmt.Fprintf(writer,"%v\n", strings[0])
+	fileInfo, err := os.Stat(fileName)
+	if err != nil {
+		fmt.Fprintf(errWriter, "%v\n", err)
+		return
+	}
+	fmt.Printf("fileName: %v, fileSize: %v\n", fileInfo.Name(), fileInfo.Size())
+
+
+	for {
+		time.Sleep(1 * time.Second)
+
+		f, err := os.Open(fileName)
+		if err != nil {
+			fmt.Fprintf(errWriter, "%v\n", err)
+			return
+		}
+
+		fileInfo, err := f.Stat()
+		if err != nil {
+			fmt.Fprintf(errWriter, "%v\n", err)
+			return
+		}
+		//fmt.Printf("fileName: %v, fileSize: %v\n", fileInfo.Name(), fileInfo.Size())
+
+		//fileInfo, err := os.Stat(fileName)
+		//if err != nil {
+		//	fmt.Fprintf(errWriter, "%v\n", err)
+		//	return
+		//}
+		//
+		size := fileInfo.Size()
+		if size > 5 {
+			fmt.Fprintf(writer, "size: %v\n", size)
+			return
+		}
+	}
+
+	read(writer, errWriter, fileName)
+}
+
+func read(writer, errWriter io.Writer, fileName string) {
+	strings, err := file.ReadFromLast(fileName)
+	if err != nil {
+		fmt.Fprintf(errWriter, "%v\n", err)
+		return
+	}
+	fmt.Fprintf(writer, "%v\n", strings[0])
 }
