264cf3f3aaec4eeb32bd0bd0db007fb916d22dab
su-kun1899 <higedrum.coz@gmail.com>
2018-10-21 15:26:16 +0900

    値監視

--- cli/channel/main.go
+++ cli/channel/main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+var foo int = 0
+
+type diff struct {
+	oldVal int
+	newVal int
+}
+
+func watchFoo(c chan int) {
+	currentVal := foo
+
+	for {
+		if currentVal != foo {
+			c <- foo
+		}
+	}
+}
+
 func main() {
-	println("hello")
+	c := make(chan int)
+
+	go watchFoo(c)
+
+	time.Sleep(3 * time.Second)
+	foo = 3
+
+	newVal := <-c
+
+	fmt.Printf("newVal: %d", newVal)
+
+	//s := []int{7, 2, 8, -9, 4, 0}
+	//
+	//c := make(chan int)
+	//go sum(s[:len(s)/2], c)
+	//go sum(s[len(s)/2:], c)
+	//x, y := <-c, <-c // receive from c
+	//
+	//fmt.Println(x, y, x+y) // -5 17 12
 }
