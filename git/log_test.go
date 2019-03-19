64d82e90d9ab2bf0119de62bd1cce3b2379818c0
su-kun1899 <higedrum.coz@gmail.com>
2019-03-19 17:39:17 +0900

    とりあえず example をまんま動かしてみる

--- git/log_test.go
+++ git/log_test.go
@@ -1,7 +1,50 @@
 package git
 
-import "testing"
+import (
+	"fmt"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/storage/memory"
+	"testing"
+)
 
 func Test(t *testing.T) {
-	println("Hello test")
+	// Clones the given repository in memory, creating the remote, the local
+	// branches and fetching the objects, exactly as:
+	Info("git clone https://github.com/src-d/go-siva")
+
+	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: "https://github.com/src-d/go-siva",
+	})
+
+	CheckIfError(err)
+
+	// Gets the HEAD history from HEAD, just like this command:
+	Info("git log")
+
+	// ... retrieves the branch pointed by HEAD
+	ref, err := r.Head()
+	CheckIfError(err)
+
+
+	// ... retrieves the commit history
+	cIter, err := r.Log(&git.LogOptions{From: ref.Hash()})
+	CheckIfError(err)
+
+	// ... just iterates over the commits, printing it
+	err = cIter.ForEach(func(c *object.Commit) error {
+		fmt.Println(c)
+		return nil
+	})
+	CheckIfError(err)
+}
+
+func CheckIfError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Info(s string) {
+	println(s)
 }
